2c67fef9152c5a9c391eff0ec7b04a3dce30beeb
Niko Escobal <niko.escobal@gmail.com>
2021-10-27 17:58:49 +0800

    Update eslint

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     // eslint-disable-next-line react/jsx-no-duplicate-props
     <div className="App" className="p-6 items-center justify-center">
-      <h1 className="text-blue-400 font-extrabold"> Hello World! </h1>
+      <h1 className="text-blue-400 font-nunito font-extrabold">
+        {' '}
+        Hello World!
+        {' '}
+      </h1>
       {' '}
       <p className="tracking-widest"> This is my first React App. </p>
       {' '}
