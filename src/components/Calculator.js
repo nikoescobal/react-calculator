707473c8e03f865572cae8791a68b63ee07a7e95
Niko Escobal <niko.escobal@gmail.com>
2021-10-28 21:39:00 +0800

    Adjust styling of buttons and add btn classes to css

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,63 +13,25 @@ class Calculator extends React.Component {
         <div className="bg-blue-500 rounded font-nunito text-2xl col-span-4 px-6 text-white items-center text-center align-middle flex justify-end">
           0
         </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          AC
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          +/-
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          %
-        </div>
-        <div className="bg-yellow-500 hover:bg-yellow-300 opacity-100 hover:opacity-80 rounded font-bold text-white hover:text-yellow-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          ÷
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          7
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          8
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          9
-        </div>
-        <div className="bg-yellow-500 hover:bg-yellow-300 opacity-100 hover:opacity-80 rounded font-bold text-white hover:text-yellow-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          x
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          4
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          5
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          6
-        </div>
-        <div className="bg-yellow-500 hover:bg-yellow-300 opacity-100 hover:opacity-80 rounded font-bold text-white hover:text-yellow-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          -
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          1
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          2
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          3
-        </div>
-        <div className="bg-yellow-500 hover:bg-yellow-300 opacity-100 hover:opacity-80 rounded font-bold text-white hover:text-yellow-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          +
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold col-span-2 hover:text-green-700 text-white items-center text-center align-middle flex justify-center cursor-pointer">
-          0
-        </div>
-        <div className="bg-green-500 opacity-100 hover:opacity-80 hover:bg-green-300 rounded font-bold text-white hover:text-green-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          .
-        </div>
-        <div className="bg-yellow-500 hover:bg-yellow-300 opacity-100 hover:opacity-80 rounded font-bold text-white hover:text-yellow-700 items-center text-center align-middle flex justify-center cursor-pointer">
-          =
-        </div>
+        <div className="btn-green">AC</div>
+        <div className="btn-green">+/-</div>
+        <div className="btn-green">%</div>
+        <div className="btn-yellow">÷</div>
+        <div className="btn-green">7</div>
+        <div className="btn-green">8</div>
+        <div className="btn-green">9</div>
+        <div className="btn-yellow">x</div>
+        <div className="btn-green">4</div>
+        <div className="btn-green">5</div>
+        <div className="btn-green">6</div>
+        <div className="btn-yellow">-</div>
+        <div className="btn-green">1</div>
+        <div className="btn-green">2</div>
+        <div className="btn-green">3</div>
+        <div className="btn-yellow">+</div>
+        <div className="btn-green col-span-2">0</div>
+        <div className="btn-green">.</div>
+        <div className="btn-yellow">=</div>
       </div>
     );
   }
