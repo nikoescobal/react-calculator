e4e3e6e48369742b417c39aa925f55b358aced88
Niko Escobal <niko.escobal@gmail.com>
2021-10-28 21:31:36 +0800

    Add components and basic styling to calculator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
-    // eslint-disable-next-line react/jsx-no-duplicate-props
-    <div className="App" className="p-6 items-center justify-center">
-      <h1 className="text-blue-400 font-nunito font-extrabold">
-        {' '}
-        Hello World!
-        {' '}
-      </h1>
-      {' '}
-      <p className="tracking-widest"> This is my first React App. </p>
-      {' '}
+    <div>
+      <Calculator />
     </div>
   );
 }
