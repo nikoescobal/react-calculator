9bece173f36c0fe3530064c233bb351f1f8d571f
Niko Escobal <niko.escobal@gmail.com>
2021-10-28 22:04:26 +0800

    Fix linters, change app function to class component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 /* eslint-disable import/no-cycle */
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div>
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
